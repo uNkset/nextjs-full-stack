c1b0f65ce371b2e78e004f1c30840055c64a26c7
Konstantin Arescenko <kostylo@windowslive.com>
2023-07-19 16:39:14 +0200

    fetch api endpoint created

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,4 +1,51 @@
+'use client'
+
+import { useState, useEffect, Key } from 'react'
+import PromptCard from './PromptCard'
+
+interface PromptCardListProps {
+  data: Record<string, any>
+  handleTagClick: Function
+}
+
+export interface PromptCardProps {
+  key?: Key
+  prompt: Record<string, any>
+  handleTagClick: Function
+}
+
+interface PromptMapProps {
+  _id: string | number
+}
+
+const PromptCardList = ({ data, handleTagClick }: PromptCardListProps) => {
+  return (
+    <div className="mt-16 prompt_layout">
+      {data.map((p: PromptMapProps) => (
+        <PromptCard key={p._id} prompt={p} handleTagClick={handleTagClick} />
+      ))}
+    </div>
+  )
+}
+
 const Feed = () => {
+  const [prompts, setPrompts] = useState([])
+
+  const [searchText, setSearchText] = useState('')
+  const [searchTimeout, setSearchTimeout] = useState(null)
+  const [searchResults, setSearchResults] = useState([])
+
+  const fetchPrompts = async () => {
+    const response = await fetch('/api/prompt')
+    const data = await response.json()
+
+    setPrompts(data)
+  }
+
+  useEffect(() => {
+    fetchPrompts()
+  }, [])
+
   return <div>Feed</div>
 }
 
