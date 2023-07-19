95ae6a803a27164e497a1c899285d2beeb6dcaee
Konstantin Arescenko <kostylo@windowslive.com>
2023-07-19 14:27:26 +0200

    types for form component

--- components/Form.tsx
+++ components/Form.tsx
@@ -1,6 +1,14 @@
 import Link from 'next/link'
 
-const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
+interface FromProps {
+  type: string
+  post: Record<string, string>
+  setPost: Function
+  submitting: boolean
+  handleSubmit: (e: any) => void // Function
+}
+
+const Form = ({ type, post, setPost, submitting, handleSubmit }: FromProps) => {
   return (
     <section className="w-full max-w-full flex-start flex-col">
       <h1 className="head_text text_left">
