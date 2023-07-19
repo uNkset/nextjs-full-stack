5936e73fc6a17c4d6b6b86d6bba56b2ed01b8080
Konstantin Arescenko <kostylo@windowslive.com>
2023-07-19 15:44:07 +0200

    new prompt api endpoint inplemented

--- app/create-prompt/page.tsx
+++ app/create-prompt/page.tsx
@@ -2,18 +2,43 @@
 
 import { useState } from 'react'
 import { useSession } from 'next-auth/react'
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/navigation'
 
 import Form from '@components/Form'
 
 const CreatePrompt = () => {
+  const router = useRouter()
+  const { data: session } = useSession()
+
   const [submitting, setSubmitting] = useState(false)
   const [post, setPost] = useState({
     prompt: '',
     tag: '',
   })
 
-  const addPrompt = async (e: any) => {}
+  const addPrompt = async (e: any) => {
+    e.preventDefault()
+    setSubmitting(true)
+
+    try {
+      const response = await fetch('/api/prompt/new', {
+        method: 'POST',
+        body: JSON.stringify({
+          userId: session?.user.id,
+          prompt: post.prompt,
+          tag: post.tag,
+        }),
+      })
+
+      if (response.ok) {
+        router.push('/')
+      }
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setSubmitting(false)
+    }
+  }
 
   return (
     <Form
