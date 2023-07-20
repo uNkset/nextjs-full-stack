95f468a088e7e2863ab0fd1bdb445f3423f0a28b
Konstantin Arescenko <kostylo@windowslive.com>
2023-07-20 14:32:36 +0200

    added logic for fetching user posts in profilepage

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,7 +1,41 @@
-import React from 'react'
+'use client'
 
-const Profile = () => {
-  return <div>Profile</div>
+import { useState, useEffect, use } from 'react'
+import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/navigation'
+
+import Profile from '@components/Profile'
+
+const MyProfile = () => {
+  const router = useRouter()
+  const { data: session } = useSession()
+  const userId = session?.user?.id
+  const [myPrompts, setMyPrompts] = useState([])
+
+  useEffect(() => {
+    const fetchMyPosts = async () => {
+      const response = await fetch(`/api/users/${userId}/posts`)
+      const data = await response.json()
+
+      setMyPrompts(data)
+    }
+
+    if (userId) fetchMyPosts()
+  }, [userId])
+
+  const handleEdit = () => {}
+
+  const handleDelete = () => {}
+
+  return (
+    <Profile
+      name="My"
+      desc="Welcome to your personalized profile page. Share your exceptional prompts and inspire other with the power of your imagination"
+      data={myPrompts}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
+  )
 }
 
-export default Profile
+export default MyProfile
