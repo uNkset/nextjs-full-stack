7e118b84642a3c7ea533630e7af06f998603904a
Konstantin Arescenko <kostylo@windowslive.com>
2023-07-20 14:32:36 +0200

    added logic for fetching user posts in profilepage

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,4 +1,18 @@
-const Profile = () => {
+interface ProfileProps {
+  name: string
+  desc: string
+  data: any
+  handleEdit: Function
+  handleDelete: Function
+}
+
+const Profile = ({
+  name,
+  desc,
+  data,
+  handleEdit,
+  handleDelete,
+}: ProfileProps) => {
   return <div>Profile</div>
 }
 
