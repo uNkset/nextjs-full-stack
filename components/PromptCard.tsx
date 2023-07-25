22a07beb3ea7a75768a78b8d26631aa88596637e
Konstantin Arescenko <kostylo@windowslive.com>
2023-07-25 19:02:34 +0200

    added edit and delete buttons in profile page

--- components/PromptCard.tsx
+++ components/PromptCard.tsx
@@ -12,6 +12,9 @@ const PromptCard = ({
   handleEdit,
   handleDelete,
 }: PromptCardProps) => {
+  const { data: session } = useSession()
+  const pathName = usePathname()
+  const router = useRouter()
   const [copied, setCopied] = useState('')
 
   const handleProfileClick = () => {}
@@ -68,6 +71,24 @@ const PromptCard = ({
       >
         {promptObj.tag}
       </p>
+
+      {session?.user?.id === promptObj.creator._id &&
+        pathName === '/profile' && (
+          <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
+            <p
+              className="font-inter text-sm green_gradient cursor-pointer"
+              onClick={handleEdit}
+            >
+              Edit
+            </p>
+            <p
+              className="font-inter text-sm orange_gradient cursor-pointer"
+              onClick={handleDelete}
+            >
+              Delete
+            </p>
+          </div>
+        )}
     </div>
   )
 }
