8beb10df3ddc3ac8485462ba60f28d172c4d4aea
Konstantin Arescenko <kostylo@windowslive.com>
2023-07-25 12:25:25 +0200

    my profile basic functionality implemented

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,4 +1,5 @@
 import { PromptObjectProps } from './Feed'
+import PromptCard from './PromptCard'
 
 interface ProfileProps {
   name: string
@@ -15,9 +16,25 @@ const Profile = ({
   handleEdit,
   handleDelete,
 }: ProfileProps) => {
-  console.log(data)
+  return (
+    <section className="w-full">
+      <h1 className="head_text text-left">
+        <span className="blue_gradient">{name} Profile</span>
+      </h1>
+      <p className="desc text-left">{desc}</p>
 
-  return <div>Profile</div>
+      <div className="mt-10 prompt_layout">
+        {data.map((p) => (
+          <PromptCard
+            key={p._id}
+            promptObj={p}
+            handleEdit={() => handleEdit && handleEdit(p)}
+            handleDelete={() => handleDelete && handleDelete(p)}
+          />
+        ))}
+      </div>
+    </section>
+  )
 }
 
 export default Profile
